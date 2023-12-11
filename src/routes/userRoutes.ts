025124050953f60ba15d0399f2e84e9c2fdc969d
Fozil Zayn <fozil.z@appliedlabs.io>
2023-12-11 08:44:41 +0500

    user crud

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,31 +1,70 @@
 import { Router } from "express"
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
-
+const prisma = new PrismaClient()
 
 // User CRUD
 
-router.post('/', (req, res) => {
-  res.status(501).json({ error: "Not Implemented" })
+router.post('/', async (req, res) => {
+  const { email, name, username, bio } = req.body
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        bio
+      }
+    })
+
+    res.json(result)
+  } catch (e) {
+    res.status(400).json({ error: "Username and email should be unique" })
+  }
 })
 
-router.get('/', (req, res) => {
-  res.status(501).json({ error: "Not Implemented" })
+router.get('/', async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+
+
+  res.json(allUsers)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ id: id })
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  })
+  res.json(user)
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ id: id })
+
+  const { bio, name, image } = req.body;
+  try {
+    const result = await prisma.user.update({
+      where: { id: Number(id) },
+      data: {
+        bio,
+        name,
+        image
+      }
+    })
+    res.json(result)
+  } catch (error) {
+    res.status(400).json({ error: "Failed to update the user" })
+  }
+
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ id: id })
+  await prisma.user.delete({ where: { id: Number(id) } })
+  res.status(200)
 })
 
 
